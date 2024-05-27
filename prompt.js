d022c5111744823140e53c68abd1145e06df639f
Christopher Giordano <chrisgcpg@gmail.com>
2024-05-27 11:05:11 -0400

    Added a function and inquirer option to update an employee's role.

--- prompt.js
+++ prompt.js
@@ -1,6 +1,6 @@
 // Separate file to handle inquirer
 const inquirer = require('inquirer');
-const { getEmployees, getDepartments, getRoles, pgp, cn, db} = require('./getFunctions');
+const { getEmployees, getDepartments, getRoles, pgp, cn, db } = require('./getFunctions');
 // Variables for all the prompts for inquirer
 const menuPrompts = [
   {
@@ -8,58 +8,70 @@ const menuPrompts = [
     message: "What would you like to do?",
     name: "menu",
     choices: ["View Employees", "View Departments", "View Roles", "Add Employee",
-      "Add Department", "Add Role", "Quit"],
+      "Add Department", "Add Role", "Update Role", "Quit"],
   }
 ]
-  const addEmployeePrompts = [
-    {
-      type: "input",
-      message: "What is the empolyees first name?",
-      name: "firstName",
-    },
-    {
-      type: "input",
-      message: "What is the empolyees last name?",
-      name: "lastName",
-    },
-    {
-      type: "input",
-      message: "What is the empolyees role id?",
-      name: "roleId",
-    },
-    {
-      type: "input",
-      message: "What is the empolyees department id?",
-      name: "departmentId",
-    }
-  ];
-  const addDepartmentPrompts = [
-    {
-      type: "input",
-      message: "What is the name of the department?",
-      name: "dep_name",
-    }
-  ];
-  const addRolePrompts = [
-    {
-        type: "input",
-        message: "What is the title of the role?",
-        name: "title",
-    },
-    {
-        type: "input",
-        messgae: "what is the salary of the role?",
-        name: "salary",
-    },
-    {
-        type: "input",
-        message: "what is the department for this role?",
-        name: "department",
-    }
-  ];
-  // A big swicth case to call different functions depending on the option slected
-  const startMenu = async () => {
-    console.log('\n')
+const addEmployeePrompts = [
+  {
+    type: "input",
+    message: "What is the empolyees first name?",
+    name: "firstName",
+  },
+  {
+    type: "input",
+    message: "What is the empolyees last name?",
+    name: "lastName",
+  },
+  {
+    type: "input",
+    message: "What is the empolyees role id?",
+    name: "roleId",
+  },
+  {
+    type: "input",
+    message: "What is the empolyees department id?",
+    name: "departmentId",
+  }
+];
+const addDepartmentPrompts = [
+  {
+    type: "input",
+    message: "What is the name of the department?",
+    name: "dep_name",
+  }
+];
+const addRolePrompts = [
+  {
+    type: "input",
+    message: "What is the title of the role?",
+    name: "title",
+  },
+  {
+    type: "input",
+    messgae: "what is the salary of the role?",
+    name: "salary",
+  },
+  {
+    type: "input",
+    message: "what is the department for this role?",
+    name: "department",
+  }
+];
+const updateEmployeeRolePrompts = [
+  {
+    type: "input",
+    message: "What is the id and the employee?",
+    name: "employeeId",
+  },
+  {
+    type: "input",
+    message: "What is the updated role id for the employee?",
+    name: "roleId",
+  }
+]
+// A big swicth case to call different functions depending on the option slected
+const startMenu = async () => {
+  console.log('\n')
   await inquirer
     .prompt(menuPrompts)
     .then((result) => {
@@ -86,6 +98,9 @@ const menuPrompts = [
         case "Add Role":
           addRole();
           break;
+        case "Update Role":
+          updateEmployeeRole();
+          break;
         case "Quit":
           console.log("Exit terminal")
         default:
@@ -95,37 +110,45 @@ const menuPrompts = [
       }
     }
     );
-  };
-  //  Functions to add data to the table, Still need to add code functionality
-  const addDepartment = async () => {
-    await inquirer
-          .prompt(addDepartmentPrompts)
-          .then((result) =>
-            db.any(`INSERT INTO department (dep_name)
+};
+//  Functions to add data to the table, Still need to add code functionality
+const addDepartment = async () => {
+  await inquirer
+    .prompt(addDepartmentPrompts)
+    .then((result) =>
+      db.any(`INSERT INTO department (dep_name)
           VALUES ('${result.dep_name}');`));
-          startMenu();
-  }
-  const addRole = async () => {
-    await inquirer
-          .prompt(addRolePrompts)
-          .then((result) => 
-          db.any(`INSERT INTO role (title, salary, department_id)
+  startMenu();
+}
+const addRole = async () => {
+  await inquirer
+    .prompt(addRolePrompts)
+    .then((result) =>
+      db.any(`INSERT INTO role (title, salary, department_id)
           VALUES ('${result.title}', ${result.salary}, ${result.department});`));
-          startMenu();
-  }
-  const addEmployee = async () => {
-    await inquirer
-          .prompt(addEmployeePrompts)
-          .then((result) =>
-            db.any(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  startMenu();
+}
+const addEmployee = async () => {
+  await inquirer
+    .prompt(addEmployeePrompts)
+    .then((result) =>
+      db.any(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
           VALUES ('${result.firstName}', '${result.lastName}', ${result.roleId}, ${result.departmentId});`));
-          startMenu();
-  }
-  // Exports functions to other javascript files
-    module.exports = {
-      menuPrompts,
-      addDepartmentPrompts,
-      addRolePrompts,
-      addEmployeePrompts,
-      startMenu,
-    };
+  startMenu();
+}
+// Function to find an employee by id and update the role_id
+const updateEmployeeRole = async () => {
+  await inquirer
+    .prompt(updateEmployeeRolePrompts)
+    .then((result) =>
+    db.any(`UPDATE employee SET role_id = ${result.roleId} WHERE employee.id = ${result.employeeId}`));
+  startMenu();
+}
+// Exports functions to other javascript files
+module.exports = {
+  menuPrompts,
+  addDepartmentPrompts,
+  addRolePrompts,
+  addEmployeePrompts,
+  startMenu,
+};
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { startMenu, menuPrompts, addDepartmentPrompts, addRolePrompts, addEmployeePrompts,} = require('./prompt.js');
+const { startMenu, menuPrompts, addDepartmentPrompts, addRolePrompts, addEmployeePrompts, } = require('./prompt.js');
 const { getEmployees, getDepartments, getRoles } = require('./getFunctions.js');
 const inquirer = require('inquirer');
 const { default: Choice } = require('inquirer/lib/objects/choice');
